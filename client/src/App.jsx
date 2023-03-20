878f29f805acb7596efab980db8b6899548f4873
sonOfArent <humbleogier@gmail.com>
2023-03-20 11:40:29 -0500

    asd

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  /*
+      My first course of action should be to create the area where selecting things happens, as well as deciding the rest of the layout.
+    I already know I want to make the site utilize minecraft hud elements. For instance, every box I can select I'd like it to use the inventory square look (I'll call them "slots". they'll take in an image and a name, and ). I think the way I'll accomplish that is by tweaking the border and background until it looks pretty similar.
+
+  */
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
