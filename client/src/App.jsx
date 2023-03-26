cfb43b64cee719add8a9263dcf302e05cbc2bfc3
sonOfArent <91688309+sonOfArent@users.noreply.github.com>
2023-03-26 16:39:41 -0500

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,31 @@ import ItemGallery from './components/ImageGallery/ItemGallery'
 
 // #TODO: generate files like ItemGallery for BlockGallery and MobGallery. I would do it now but by god I need to sleep.
 
+// I forgot to also upload the application to actually create the gallery data, so something else I will have to find.
+
 function App() {
+
+  const [itemsEnabled, setItemsEnabled] = useState({})
+
+  const updateItemsEnabled = (key, newValue) => {
+    setItemsEnabled(previous => ({
+      ...previous,
+      [key]: newValue
+    }))
+  }
 
   return (
     <div className="App">
       <div className="Slot-windows">
         <Window type="Items">
-          <ItemGallery /> 
+          <ItemGallery updateItemsEnabled={updateItemsEnabled} />
         </Window>
         <Window type="Blocks">
         </Window>
         <Window type="Mobs">
 
         </Window>
+        <button onClick={() => console.log(itemsEnabled)}></button>
       </div>
       <div className="Settings-window">
         Settings box
--- client/src/components/Slot/Slot.jsx
+++ client/src/components/Slot/Slot.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import './Slot.css'
 
-// It would be easier to check if the individual slot is clicked here instead of higher up the chain.
-
-// I think the best course of action is to give each slot the ability to determine if it's enabled, and when submitted, a process above will check with every slot and see which were enabled and which were not.
-
-const Slot = ({ image }) => {
+const Slot = ({ id, image, updateItemsEnabled }) => {
 
   const [isEnabled, setIsEnabled] = useState(false)
 
+  useEffect(() => {
+    updateItemsEnabled(id, isEnabled)
+  }, [isEnabled])
+
+  const handleEnabledChange = () => {
+    setIsEnabled(!isEnabled)
+  }
+
   return (
-    <div className={`Slot ${isEnabled}`} onClick={() => setIsEnabled(!isEnabled)}>
+    <div className={`Slot ${isEnabled}`} onClick={() => handleEnabledChange()}>
       <div className="container image">
         <img src={image || ""} alt="" srcset="" key={image} />
       </div>
